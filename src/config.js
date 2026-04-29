149957ae68894e354ea82ca3cd9f45bfe9a2b9ce
justrhoto <otisub@gmail.com>
2026-04-29 09:53:14 -0500

    Feat: Get rid of locally encoded admin and use Manage Guild perms

--- src/config.js
+++ src/config.js
@@ -1,8 +1,3 @@
 module.exports = {
   token: process.env.DISCORD_TOKEN || 'YOUR_BOT_TOKEN_HERE',
-  // Comma-separated Discord user IDs allowed to use bot commands.
-  // Leave empty to allow everyone (subject to Discord permission checks).
-  adminUserIds: process.env.ADMIN_USER_IDS
-    ? process.env.ADMIN_USER_IDS.split(',').map(id => id.trim()).filter(Boolean)
-    : [],
 };
